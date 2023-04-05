ec2414460e014b9774270b4240833db0bed1fb9d
Simon Aron <simonarons15@gmail.com>
2023-04-05 21:23:58 +0300

    Add unlockable support

---
+++ bluedom-be/bluedom-be/Controllers/UnlockableController.cs
@@ -0,0 +1,35 @@
+﻿using bluedom_be.Models;
+using bluedom_be.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bluedom_be.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UnlockableController : ControllerBase
+{
+    private readonly UnlockableService _unlockableService;
+    
+    public UnlockableController(UnlockableService unlockableService)
+    {
+        _unlockableService = unlockableService;
+    }
+
+    [HttpGet]
+    public async Task<List<Unlockable>> Get()
+    {
+        return await _unlockableService.GetAsync();
+    }
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Unlockable>> Get(string id)
+    {
+        var unlockable = await _unlockableService.GetAsync(id);
+        if (unlockable is null)
+        {
+            return NotFound();
+        }
+
+        return unlockable;
+    }
+}
