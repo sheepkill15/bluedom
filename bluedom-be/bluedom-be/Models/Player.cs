1e5c3b8830b25cb17cf48802f7881ec6f1d620b3
Simon Aron <simonarons15@gmail.com>
2023-04-05 16:06:20 +0300

    Add User, Player, Quest, Badge, Unlockable models

---
+++ bluedom-be/bluedom-be/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace bluedom_be.Models;
+
+public class Player
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Quests { get; set; }
+    
+    public int Tokens { get; set; }
+
+    public List<string> Badges { get; set; } = null!;
+    public List<string> Purchases { get; set; } = null!;
+}
---
+++ bluedom-be/bluedom-be/Models/Unlockable.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace bluedom_be.Models;
+
+public enum UnlockableType
+{
+    [BsonRepresentation(BsonType.String)]
+    Background,
+    [BsonRepresentation(BsonType.String)]
+    Font,
+    [BsonRepresentation(BsonType.String)]
+    Effect
+}
+
+public class Unlockable
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public UnlockableType Type { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+    
+    public string Data { get; set; } = null!;
+    
+    public int Cost { get; set; }
+}
