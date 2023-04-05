4da0782b77a3801f01ceb98f226108c0f9f68c11
Simon Aron <simonarons15@gmail.com>
2023-04-05 16:07:07 +0300

    Add store settings model

---
+++ bluedom-be/bluedom-be/Models/BluedomStoreDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace bluedom_be.Models;
+
+public class BluedomStoreDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string PlayersCollectionName { get; set; } = null!;
+    public string BadgesCollectionName { get; set; } = null!;
+    public string QuestsCollectionName { get; set; } = null!;
+    public string UnlockablesCollectionName { get; set; } = null!;
+    public string UsersCollectionName { get; set; } = null!;
+}
