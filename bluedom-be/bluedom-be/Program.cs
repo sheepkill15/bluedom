77e94fb23fb6bf73ac037674cfc7990e0a833a50
Simon Aron <simonarons15@gmail.com>
2023-04-05 16:07:19 +0300

    Register services and settings

--- bluedom-be/bluedom-be/Program.cs
+++ bluedom-be/bluedom-be/Program.cs
@@ -1,6 +1,16 @@
+using bluedom_be.Models;
+using bluedom_be.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BluedomStoreDatabaseSettings>(builder.Configuration.GetSection("BluedomStoreDatabase"));
+
+builder.Services.AddSingleton<PlayerService>();
+builder.Services.AddSingleton<QuestService>();
+builder.Services.AddSingleton<BadgeService>();
+builder.Services.AddSingleton<UnlockableService>();
+builder.Services.AddSingleton<UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
