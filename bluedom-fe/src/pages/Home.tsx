72c6ff97d82004862dac8b8c97bbe4eb3bea7bc2
Simon Aron <simonarons15@gmail.com>
2023-04-06 20:29:31 +0300

    basic quest list

--- bluedom-fe/src/pages/Home.tsx
+++ bluedom-fe/src/pages/Home.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Table from "react-bootstrap/Table";
+import { API_BASEURL, sendGetRequest } from "../Api";
+import QuestItem, {Quest} from "../components/Quest";
 
 const Home = () => {
+
+    const [quests, setQuests] = useState<Quest[]>([]);
+
+    const fetchAllQuests = () => {
+        sendGetRequest(`${API_BASEURL}/Quest`, {})
+            .then((res) => res.json())
+            .then(setQuests)
+            .catch(console.log);
+    }
+
+    useEffect(fetchAllQuests, [])
+
     return (
-        <div>At home.</div>
+        <Table variant="dark">
+            <thead>
+                <tr>
+
+                <th>
+                    Name of quest
+                </th>
+                <th>
+                    Length of text
+                </th>
+                <th>
+                    Reward
+                </th>
+                <th>
+                    Required time to complete
+                </th>
+                <th>
+                    Issued by
+                </th>
+                <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {quests.map((quest) => <QuestItem quest={quest} />)}
+            </tbody>
+        </Table>
     )
 }
 
