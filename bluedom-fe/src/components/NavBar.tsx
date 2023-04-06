7d9e48fd7393e7fb3a5a15b7d3fbfac10a445032
Simon Aron <simonarons15@gmail.com>
2023-04-06 21:20:42 +0300

    add margin below navbar

--- bluedom-fe/src/components/NavBar.tsx
+++ bluedom-fe/src/components/NavBar.tsx
@@ -1,41 +1,56 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import { NavLink } from "react-router-dom";
-import { UserContext } from "../User";
+import { NavLink } from 'react-router-dom';
+import { UserContext } from '../User';
 
 const NavBar = () => {
-    const userContext = useContext(UserContext);
+  const userContext = useContext(UserContext);
 
-    const logout = () => {
-        userContext.setUser({
-            loggedIn: false,
-            username: '',
-            playerId: ''
-        });
-    }
+  const logout = () => {
+    userContext.setUser({
+      loggedIn: false,
+      username: '',
+      playerId: '',
+    });
+  };
 
-    return (
-        <Navbar bg="dark" expand="sm" variant="dark">
-            <Container>
-                <Navbar.Brand>Bluedom</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={NavLink} to="home">Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="leaderboard">Leaderboard</Nav.Link>
-                        <Nav.Link as={NavLink} to="shop">Shop</Nav.Link>
-                    </Nav>
-                    {userContext.user.loggedIn && <Nav className="justify-content-end">
-                        <Navbar.Text>{userContext.user.username}</Navbar.Text> 
-                        <Nav.Link as={Button} variant="link" onClick={logout}>Logout</Nav.Link>
-                    </Nav>}
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar
+      style={{ marginBottom: '2rem' }}
+      bg="dark"
+      expand="sm"
+      variant="dark"
+    >
+      <Container>
+        <Navbar.Brand>Bluedom</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="leaderboards">
+              Leaderboards
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="shop">
+              Shop
+            </Nav.Link>
+          </Nav>
+          {userContext.user.loggedIn && (
+            <Nav className="justify-content-end">
+              <Navbar.Text>{userContext.user.username}</Navbar.Text>
+              <Nav.Link as={Button} variant="link" onClick={logout}>
+                Logout
+              </Nav.Link>
+            </Nav>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
