a480ab8b8d7f805ea4b161919e444be435635a94
Simon Aron <simonarons15@gmail.com>
2023-04-06 21:19:55 +0300

    allow params as optional for get request

--- bluedom-fe/src/Api.tsx
+++ bluedom-fe/src/Api.tsx
@@ -1,9 +1,14 @@
-export const API_BASEURL = 'https://localhost:7148/api'
+export const API_BASEURL = 'https://localhost:7148/api';
 
-export const sendGetRequest = (url: string, params: {[id: string]: string}) => {
-    const _url = new URL(url);
+export const sendGetRequest = (
+  url: string,
+  params?: { [id: string]: string }
+) => {
+  const _url = new URL(url);
 
+  if (params) {
     _url.search = new URLSearchParams(params).toString();
-    
-    return fetch(_url);
-}
+  }
+
+  return fetch(_url);
+};
