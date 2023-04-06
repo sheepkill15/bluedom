be736d7bf2659d12fd7bc3777eb35fa177dcb3bc
Simon Aron <simonarons15@gmail.com>
2023-04-06 20:54:38 +0300

    show basic statistics for logged in player

---
+++ bluedom-fe/src/components/PlayerStats.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+export type Player = {
+  name: string;
+  quests: number;
+  tokens: number;
+  badges: string[];
+  purchases: string[];
+};
+
+const PlayerStats = ({ player }: { player: Player }) => {
+  return (
+    <Card bg="dark" text="white">
+      <Card.Body>
+        <Card.Title>{player.name}</Card.Title>
+        <Card.Text>{player.tokens} tokens</Card.Text>
+        <Card.Text>{player.quests} quests completed</Card.Text>
+        <Card.Text>{player.badges?.length ?? 0} badges acquired</Card.Text>
+        <Card.Text>{player.purchases?.length ?? 0} purchases</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default PlayerStats;
--- bluedom-fe/src/pages/Home.tsx
+++ bluedom-fe/src/pages/Home.tsx
@@ -1,49 +1,65 @@
-import React, {useState, useEffect} from "react";
-import Table from "react-bootstrap/Table";
-import { API_BASEURL, sendGetRequest } from "../Api";
-import QuestItem, {Quest} from "../components/Quest";
+import React, { useState, useEffect, useContext } from 'react';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import { API_BASEURL, sendGetRequest } from '../Api';
+import QuestItem, { Quest } from '../components/Quest';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import PlayerStats, { Player } from '../components/PlayerStats';
+import { UserContext } from '../User';
 
 const Home = () => {
+  const userContext = useContext(UserContext);
 
-    const [quests, setQuests] = useState<Quest[]>([]);
+  const [quests, setQuests] = useState<Quest[]>([]);
 
-    const fetchAllQuests = () => {
-        sendGetRequest(`${API_BASEURL}/Quest`, {})
-            .then((res) => res.json())
-            .then(setQuests)
-            .catch(console.log);
-    }
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
-    useEffect(fetchAllQuests, [])
+  const fetchAllQuests = () => {
+    sendGetRequest(`${API_BASEURL}/Quest`, {})
+      .then((res) => res.json())
+      .then(setQuests)
+      .catch(console.log);
+  };
 
-    return (
-        <Table variant="dark">
+  const fetchUserAssociatedPlayer = () => {
+    sendGetRequest(`${API_BASEURL}/Player/${userContext.user.playerId}`, {})
+      .then((res) => res.json())
+      .then(setCurrentPlayer)
+      .catch(console.log);
+  };
+
+  useEffect(fetchAllQuests, []);
+  useEffect(fetchUserAssociatedPlayer, [userContext.user]);
+
+  return (
+    <Container style={{ marginTop: '2rem' }}>
+      <Row>
+        <Col>
+          <Table variant="dark">
             <thead>
-                <tr>
-
-                <th>
-                    Name of quest
-                </th>
-                <th>
-                    Length of text
-                </th>
-                <th>
-                    Reward
-                </th>
-                <th>
-                    Required time to complete
-                </th>
-                <th>
-                    Issued by
-                </th>
+              <tr>
+                <th>Name of quest</th>
+                <th>Length of text</th>
+                <th>Reward</th>
+                <th>Required time to complete</th>
+                <th>Issued by</th>
                 <th></th>
-                </tr>
+              </tr>
             </thead>
             <tbody>
-                {quests.map((quest) => <QuestItem quest={quest} />)}
+              {quests.map((quest) => (
+                <QuestItem quest={quest} />
+              ))}
             </tbody>
-        </Table>
-    )
-}
+          </Table>
+        </Col>
+        <Col md="auto">
+          {currentPlayer && <PlayerStats player={currentPlayer} />}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Home;
