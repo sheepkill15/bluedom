8258792ce49ef4d2af329a5c90e0adcc5d330fa8
Simon Aron <simonarons15@gmail.com>
2023-04-09 21:17:05 +0300

    add attempt to quest item

--- bluedom-fe/src/components/Quest.tsx
+++ bluedom-fe/src/components/Quest.tsx
@@ -2,37 +2,35 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 export type Quest = {
-    id: string;
-    name: string;
-    text: string;
-    issuerName: string;
-    reward: number;
-    requiredTime: number;
-}
+  id: string;
+  name: string;
+  text: string;
+  issuerName: string;
+  reward: number;
+  requiredTime: number;
+};
 
-const QuestItem = ({quest}: {quest: Quest}) => {
-    return (
-        <tr>
-            <td>
-                {quest.name}
-            </td>
-            <td>
-                {quest.text.length} characters
-            </td>
-            <td>
-                {quest.reward} tokens
-            </td>
-            <td>
-                {quest.requiredTime}s
-            </td>
-            <td>
-                {quest.issuerName ? quest.issuerName : 'the house'}
-            </td>
-            <td>
-                <Button variant="success" size="sm">Attempt</Button>
-            </td>
-        </tr>
-    )
-}
+const QuestItem = ({
+  quest,
+  onAttempt,
+}: {
+  quest: Quest;
+  onAttempt: (quest: Quest) => void;
+}) => {
+  return (
+    <tr>
+      <td>{quest.name}</td>
+      <td>{quest.text.length} characters</td>
+      <td>{quest.reward} tokens</td>
+      <td>{quest.requiredTime}s</td>
+      <td>{quest.issuerName ? quest.issuerName : 'the house'}</td>
+      <td>
+        <Button variant="success" size="sm" onClick={() => onAttempt(quest)}>
+          Attempt
+        </Button>
+      </td>
+    </tr>
+  );
+};
 
 export default QuestItem;
