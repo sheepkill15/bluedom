31aa91e1488c23cd69dc1b7ccb3fbed1a2c0bafe
Simon Aron <simonarons15@gmail.com>
2023-04-09 21:49:30 +0300

    implement buying from the shop

--- bluedom-fe/src/pages/Shop.tsx
+++ bluedom-fe/src/pages/Shop.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { API_BASEURL, sendGetRequest } from '../Api';
+import { API_BASEURL, sendGetRequest, sendPostRequest } from '../Api';
+import { UserContext } from '../User';
 
 export enum UnlockableTypes {
   BACKGROUND = 0,
@@ -25,6 +26,9 @@ const unlockableTypeToString = ['Background', 'Font', 'Effect'];
 
 const Shop = () => {
   const [shopItems, setShopItems] = useState<Unlockable[]>([]);
+  const [ownItems, setOwnItems] = useState<string[]>([]);
+
+  const userContext = useContext(UserContext);
 
   const fetchAllShopItems = () => {
     sendGetRequest(`${API_BASEURL}/Unlockable`)
@@ -33,20 +37,45 @@ const Shop = () => {
       .catch(console.log);
   };
 
+  const fetchOwnItems = () => {
+    sendGetRequest(`${API_BASEURL}/Player/${userContext.user.playerId}`)
+      .then((res) => res.json())
+      .then((json) => setOwnItems(json['purchases'] ?? []))
+      .catch(console.log);
+  };
+
   useEffect(fetchAllShopItems, []);
+  useEffect(fetchOwnItems, [userContext.user]);
+
+  const buyItem = (item: Unlockable) => {
+    sendPostRequest(
+      `${API_BASEURL}/Unlockable/${item.id}`,
+      {},
+      { playerId: userContext.user.playerId }
+    )
+      .then((res) => res.json())
+      .then((json) => setOwnItems([...ownItems, json['id']]))
+      .catch(console.log);
+  };
 
   return (
     <Container>
       <h2>Buy stuff here</h2>
       <Row>
         {shopItems.map((item) => (
-          <Col key={item.name} md="auto">
+          <Col key={item.id} md="auto">
             <Card bg="dark" text="white">
               <Card.Body>
                 <Card.Title>{item.name}</Card.Title>
                 <Card.Text>{item.description}</Card.Text>
                 <Card.Text>Type: {unlockableTypeToString[item.type]}</Card.Text>
-                <Button variant="danger">Buy for {item.cost} tokens</Button>
+                {ownItems.find((x) => x === item.id) ? (
+                  <Button variant="success">Already bought</Button>
+                ) : (
+                  <Button variant="danger" onClick={() => buyItem(item)}>
+                    Buy for {item.cost} tokens
+                  </Button>
+                )}
               </Card.Body>
             </Card>
           </Col>
