b3ebf04a322329630433fba331cee364f16980be
Simon Aron <simonarons15@gmail.com>
2023-04-09 21:17:54 +0300

    fix always homepage

--- bluedom-fe/src/pages/Root.tsx
+++ bluedom-fe/src/pages/Root.tsx
@@ -1,31 +1,31 @@
-import React, {useEffect, useState} from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-import NavBar from '../components/NavBar'
-import { User, UserContext } from '../User'
-import Login from './Login'
+import React, { useEffect, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import { User, UserContext } from '../User';
+import Login from './Login';
 
 const Root = () => {
-    const [user, setUser] = useState<User>({
-        username: '',
-        playerId: '',
-        loggedIn: false
-    });
+  const [user, setUser] = useState<User>({
+    username: '',
+    playerId: '',
+    loggedIn: false,
+  });
 
-    const navigate = useNavigate();
-    useEffect(() => {
-        if(user.loggedIn) {
-            navigate('/home');
-        }
-    }, [user, navigate])
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user.loggedIn) {
+      navigate('/home');
+    }
+  }, [user]);
 
-    return (
-        <div>
-            <UserContext.Provider value={{user, setUser}}>
-                <NavBar />
-                {user.loggedIn ? <Outlet /> : <Login />}
-            </UserContext.Provider>
-        </div>
-    )
-}
+  return (
+    <div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <NavBar />
+        {user.loggedIn ? <Outlet /> : <Login />}
+      </UserContext.Provider>
+    </div>
+  );
+};
 
 export default Root;
