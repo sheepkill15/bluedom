67b0a176fb098cf123cfbc38f3dc2b5f627cb6a2
Simon Aron <simonarons15@gmail.com>
2023-04-10 11:41:38 +0300

    fetch issuer name on quests

--- bluedom-fe/src/components/Quest.tsx
+++ bluedom-fe/src/components/Quest.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Button } from 'react-bootstrap';
+import { API_BASEURL, sendGetRequest } from '../Api';
+import { UserContext } from '../User';
 
 export type Quest = {
   id: string;
   name: string;
   text: string;
-  issuerName: string;
+  issuerId: string;
   reward: number;
   requiredTime: number;
 };
@@ -17,15 +19,35 @@ const QuestItem = ({
   quest: Quest;
   onAttempt: (quest: Quest) => void;
 }) => {
+  const userContext = useContext(UserContext);
+
+  const [issuer, setIssuer] = useState('');
+
+  useEffect(() => {
+    if (!quest.issuerId) {
+      setIssuer('the house');
+      return;
+    }
+    sendGetRequest(`${API_BASEURL}/Player/${quest.issuerId}`)
+      .then((res) => res.json())
+      .then((player) => setIssuer(player['name']))
+      .catch(console.log);
+  }, [quest]);
+
   return (
     <tr>
       <td>{quest.name}</td>
       <td>{quest.text.length} characters</td>
       <td>{quest.reward} tokens</td>
       <td>{quest.requiredTime}s</td>
-      <td>{quest.issuerName ? quest.issuerName : 'the house'}</td>
+      <td>{issuer}</td>
       <td>
-        <Button variant="success" size="sm" onClick={() => onAttempt(quest)}>
+        <Button
+          disabled={quest.issuerId === userContext.user.playerId}
+          variant="success"
+          size="sm"
+          onClick={() => onAttempt(quest)}
+        >
           Attempt
         </Button>
       </td>
