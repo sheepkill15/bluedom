84aed3508f2c016b1caf80bb543f8affb69a410f
Simon Aron <simonarons15@gmail.com>
2023-04-11 10:30:52 +0300

    cleanup

--- bluedom-fe/src/User.tsx
+++ bluedom-fe/src/User.tsx
@@ -1,21 +1,21 @@
-import React, { createContext } from "react";
+import { createContext } from 'react';
 
 export type User = {
-    username: string;
-    playerId: string;
-    loggedIn: boolean;
+  username: string;
+  playerId: string;
+  loggedIn: boolean;
 };
 
 export type UserContextType = {
-    user: User;
-    setUser: (user: User) => void;
+  user: User;
+  setUser: (user: User) => void;
 };
 
 export const UserContext = createContext<UserContextType>({
-    user: {
-        username: '',
-        playerId: '',
-        loggedIn: false
-    },
-    setUser: () => {}
+  user: {
+    username: '',
+    playerId: '',
+    loggedIn: false,
+  },
+  setUser: () => {},
 });
